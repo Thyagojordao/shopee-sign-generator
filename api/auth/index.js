571b0d54065eb465c900cea2f8467f6524c51108
Thyagojordao <thyagojordao@gmail.com>
2025-06-03 17:56:13 -0300

    Update index.js

--- api/auth/index.js
+++ api/auth/index.js
@@ -3,19 +3,18 @@ const crypto = require('crypto');
 export default function handler(req, res) {
   const { partner_id, path, timestamp } = req.query;
 
-  // Validação dos parâmetros obrigatórios
   if (!partner_id || !path || !timestamp) {
     return res.status(400).json({ error: 'Missing parameters' });
   }
 
-  // Partner key em hexadecimal (produzido pelo painel da Shopee)
-  const partner_key = '4f415055726e715a63554872747673427a5373646244726e5841694874587666';
+  // Agora usamos Buffer direto na criação
+  const partner_key = Buffer.from('4f415055726e715a63554872747673427a5373646244726e5841694874587666', 'hex');
 
   try {
     const baseString = `${partner_id}${path}${timestamp}`;
 
     const sign = crypto
-      .createHmac('sha256', Buffer.from(partner_key.toLowerCase().trim(), 'hex'))
+      .createHmac('sha256', partner_key)
       .update(baseString)
       .digest('hex');
 
